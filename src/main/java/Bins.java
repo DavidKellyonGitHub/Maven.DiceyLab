69d768b50459bf0b4d2678d8d9137cd27e460be6
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-19 16:23:22 -0500

    Bins Working, needs tests

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,15 +1,23 @@
 
 public class Bins {
 int binNum;
-int[] binRange;
+int[] contents;
 
     public Bins(int binNumber){
         this.binNum = binNumber;
+        this.contents = new int[]{0};
     }
 
     public Bins(int lowRoll, int highRoll){
-        this.binRange = new int[]{lowRoll,highRoll};
+
+        this.contents = new int[(highRoll-lowRoll)+1];
+        for (int i = 0;i<this.contents.length;i++){
+            this.contents[i]=0;
+
+        }
     }
 
-    
+    public int getBin(int binNum){
+        return contents[binNum];
+    }
 }
